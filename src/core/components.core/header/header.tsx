c47773888d4d6c1c0fa60230e1712f983ca6af02
rocioRichi <paragit@gmail.com>
2023-04-10 20:12:54 +0200

    Add header links to github and index

--- src/core/components.core/header/header.tsx
+++ src/core/components.core/header/header.tsx
@@ -1,16 +1,24 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 export function Header() {
+  const handleClick = () => {
+    window.open("https://github.com/rocioRichi", "_blank");
+  };
+
   const urlBlueGitHub =
     "https://firebasestorage.googleapis.com/v0/b/coders2023-finalproject.appspot.com/o/micv%2FRecurso%204.svg?alt=media&token=eaed496c-e114-41f7-ab0c-9b4191bc6003";
   return (
     <>
       <header className="header">
-        Rocio Ricciardiello
+        <Link to={"/App"}>Rocio Ricciardiello</Link>
+
         <img
+          onClick={handleClick}
           src={urlBlueGitHub}
           className="header__avatargithub"
           alt="Git Hub logo"
         />
+
         <hr className="header__hr" />
       </header>
     </>
