5f9d37b2f88e61cb28484f2751162b884cad9404
rocioRichi <paragit@gmail.com>
2023-04-10 13:38:58 +0200

    Add aside, github logo and dotlines

--- src/core/components.core/app/app.tsx
+++ src/core/components.core/app/app.tsx
@@ -2,36 +2,61 @@ import "./app.css";
 
 export default function App() {
   const urlAvatar =
-    "https://firebasestorage.googleapis.com/v0/b/coders2023-finalproject.appspot.com/o/micv%2Fmiavatarb%26w.png?alt=media&token=b60c36a0-a7bd-4078-b891-3e96521f22d1";
+    "https://firebasestorage.googleapis.com/v0/b/coders2023-finalproject.appspot.com/o/micv%2Favat_comp_sonrisa_color.png?alt=media&token=0b0b50cb-fe5b-4e7c-a796-bc69bd3b1b9e";
+
+  const urlBlueGitHub =
+    "https://firebasestorage.googleapis.com/v0/b/coders2023-finalproject.appspot.com/o/micv%2FRecurso%204.svg?alt=media&token=eaed496c-e114-41f7-ab0c-9b4191bc6003";
   return (
-    <main className="container__container">
-      <section className="container__floor3">
-        <article className="container__floor3contact">Contact</article>
-        <article className="container__floor3Technologies">
-          Technologies
-        </article>
-      </section>
-      <section className="container__floor2">
-        <div className="container__floor2lang_exp">
-          <article className="container__floor2lang, bubble">Language</article>
-          <article className="container__floor2workingexp">Working Exp</article>
-        </div>
-
-        <div className="container__floor2stage">
-          <article>
-            <img src={urlAvatar} className="avatarimage" alt="Rocio`s Avatar" />
+    <div className="conteinseverything">
+      <aside className="container__asideup">Rocio Ricciardiello</aside>
+      <img
+        src={urlBlueGitHub}
+        className="container__avatargithub"
+        alt="Rocio`s Avatar"
+      />
+      <hr className="container__hrup" />
+      <main className="container__container">
+        <section className="container__floor3">
+          <article className="container__floor3contact">Contact</article>
+          <article className="container__floor3Technologies">
+            Technologies
           </article>
-        </div>
+        </section>
+        <section className="container__floor2">
+          <div className="container__floor2lang_exp">
+            <article className="container__floor2lang, bubble">
+              Soft Skills
+            </article>
+            <article className="container__floor2workingexp">
+              Working
+              <br /> Experience
+            </article>
+          </div>
 
-        <div className="container__floor2softskil_studies">
-          <article className="container__floor2softskill">Soft Skill</article>
-          <article className="container__floor2studies">Studies</article>
-        </div>
-      </section>
-      <section className="container__floor1">
-        <article className="container__floor1hobbies">Hobbies</article>
-        <article className="container__floor1whoIam">Who I am</article>
-      </section>
-    </main>
+          <div className="container__floor2stage">
+            <article>
+              <img
+                src={urlAvatar}
+                className="avatarimage"
+                alt="Rocio`s Avatar"
+              />
+            </article>
+          </div>
+
+          <div className="container__floor2softskil_studies">
+            <article className="container__floor2softskill">Education</article>
+            <article className="container__floor2studies">
+              Programing <br /> Exercise
+            </article>
+          </div>
+        </section>
+        <section className="container__floor1">
+          <article className="container__floor1hobbies">Hobbies</article>
+          <article className="container__floor1whoIam">Who I am</article>
+        </section>
+      </main>
+      <hr className="container__hrdown" />
+      <aside className="container__asidedown"></aside>
+    </div>
   );
 }
