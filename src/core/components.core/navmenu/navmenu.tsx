26f0e2587380ae1cd77b18440acd5bd97af2781d
rocioRichi <paragit@gmail.com>
2023-04-11 17:52:09 +0200

    Add navmenu code and css

--- src/core/components.core/navmenu/navmenu.tsx
+++ src/core/components.core/navmenu/navmenu.tsx
@@ -1,7 +1,35 @@
+import { EdgeLayer } from "../edgelayer/edgelayer";
+import { LeftLayer } from "../leftlayer/leftlayer";
+import { Picture } from "../picture/picture";
+import { RightLayer } from "../rightlayer/rightlayer";
 import "./navmenu.css";
 
-export function Navmenu(){
-  return(
+export function Navmenu() {
+  return (
+    <>
+      <EdgeLayer
+        firstElement={"Contact"}
+        secondElement={"Technologies"}
+      ></EdgeLayer>
+      <section className="middlelayer">
+        <LeftLayer
+          firstElement={"Soft Skills"}
+          secondElement={"Working Experience"}
+        ></LeftLayer>
 
-  )
+        <article>
+          <Picture></Picture>
+        </article>
+
+        <RightLayer
+          firstElement={"Education"}
+          secondElement={`Programming  Exercise`}
+        ></RightLayer>
+      </section>
+      <EdgeLayer
+        firstElement={"Hobbies"}
+        secondElement={"Who I am"}
+      ></EdgeLayer>
+    </>
+  );
 }
