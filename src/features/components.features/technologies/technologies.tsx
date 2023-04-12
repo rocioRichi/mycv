9d8273d53106e6507ae794fa631ca5b61b4e9ff5
rocioRichi <paragit@gmail.com>
2023-04-12 23:53:11 +0200

    Add technologies component

--- src/features/components.features/technologies/technologies.tsx
+++ src/features/components.features/technologies/technologies.tsx
@@ -2,16 +2,23 @@ import "./technologies.css";
 
 export function Technologies() {
   return (
-    <ul>
-      <li>Javascript - TypeScript </li>
-      <li></li>
-      <li>HTML , CSS, BEM, Sass</li>
-      <li>Control de versiones con GIT</li>
-      <li>ReactJS Arquitectura REDUX </li>
-      <li>Mongo DB</li>
-      <li>Principios SOLID</li>
-      <li>Principios SOLID</li>
-      <li>Adobe Premier</li>
-    </ul>
+    <section className="technologies">
+      <ul className="technologies__ul">
+        <li>Javascript - TypeScript </li>
+        <li>HTML , CSS, Sass, Figma</li>
+        <li>GIT</li>
+        <li>ReactJS Arquitectura REDUX </li>
+        <li>Mongo DB</li>
+        <li>Adobe Premier, Photoshop</li>
+      </ul>
+      <ul className="technologies__ul">
+        <li>Agile</li>
+        <li>Scrum</li>
+        <li>SOLID</li>
+        <li>BEM </li>
+        <li>Trello</li>
+        <li>Discord, Slack</li>
+      </ul>
+    </section>
   );
 }
