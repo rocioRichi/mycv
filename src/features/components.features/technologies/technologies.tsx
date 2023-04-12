44d3791eff3459f0513c7209adb8edd8d9755500
rocioRichi <paragit@gmail.com>
2023-04-12 21:26:46 +0200

    Add folder and tech comp code

---
+++ src/features/components.features/technologies/technologies.tsx
@@ -0,0 +1,17 @@
+import "./technologies.css";
+
+export function Technologies() {
+  return (
+    <ul>
+      <li>Javascript - TypeScript </li>
+      <li></li>
+      <li>HTML , CSS, BEM, Sass</li>
+      <li>Control de versiones con GIT</li>
+      <li>ReactJS Arquitectura REDUX </li>
+      <li>Mongo DB</li>
+      <li>Principios SOLID</li>
+      <li>Principios SOLID</li>
+      <li>Adobe Premier</li>
+    </ul>
+  );
+}
