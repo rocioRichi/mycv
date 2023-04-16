f89622e7e9e611b79b575a06edb228d549b99821
rocioRichi <paragit@gmail.com>
2023-04-16 10:32:51 +0200

    Add hobbies css app and delete non use files

--- src/core/components.core/approuter/approuter.tsx
+++ src/core/components.core/approuter/approuter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Contact from "../../../features/components.features/contactform/contactform";
 import { Education } from "../../../features/components.features/education/education";
+import { Hobbies } from "../../../features/components.features/hobbies/hobbies";
 import { Language } from "../../../features/components.features/language/language";
 import { Technologies } from "../../../features/components.features/technologies/technologies";
 import { InfoDisplay } from "../infodisplay/infodisplay";
@@ -13,6 +14,7 @@ export function AppRouter() {
       <Route path="/technologies" element={<Technologies />}></Route>
       <Route path="/education" element={<Education />}></Route>
       <Route path="/language" element={<Language />}></Route>
+      <Route path="/hobbies" element={<Hobbies />}></Route>
     </Routes>
   );
 }
